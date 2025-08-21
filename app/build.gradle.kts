a5bb2e485d53f88e36c8863d8bb03e26632b9277
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-08-21 01:32:52 -0400

    Se añade dependencia airbnb.lottie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
 
+    implementation("com.airbnb.lottie:lottie-compose:5.3.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
