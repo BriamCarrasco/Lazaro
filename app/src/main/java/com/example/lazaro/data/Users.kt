b876459c725d36d649858c98233c55f1fb95f0d0
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-08-22 19:40:44 -0400

    Se genera funcion con usuarios de prueba

--- app/src/main/java/com/example/lazaro/data/Users.kt
+++ app/src/main/java/com/example/lazaro/data/Users.kt
@@ -22,5 +22,19 @@ object UsersRepository {
     fun getUsers(): List<Users> {
         return usersList.toList()
     }
+
+    fun usersPrecargados(){
+        usersList.addAll(
+            listOf(
+                Users(1, "juanp", "Juan", "Perez", "Lopez", "jperezl@email.com", "qwerty123"),
+                Users(2, "rominac", "Romina", "Carrasco", "Gonzalez", "rcarrascog@email.com", "qwerty123"),
+                Users(3, "ivang", "Ivan", "Godoy", "Valedebenito", "Igodoyv@email.com", "qwerty123"),
+                Users(2, "briamc", "Briam", "Carrasco", "Fuentes", "bcarrascof@email.com", "qwerty123"),
+                Users(3, "lazaror", "Lazaro", "Ramirez", "Benavente", "lramirezb@email.com", "qwerty123"),
+
+                )
+        )
+    }
+
 }
 
