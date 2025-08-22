23eec01ad37ca93bea55879cc7f8f61fc730a863
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-08-22 00:15:19 -0400

    Se agrega en MainActivity LoginScreen y RegisterScreen

--- app/src/main/java/com/example/lazaro/MainActivity.kt
+++ app/src/main/java/com/example/lazaro/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.example.lazaro.nav.NavRouter
 import com.example.lazaro.ui.theme.LazaroTheme
-import com.example.lazaro.screens.LoginScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.lazaro.feature.login.loginScreen
+import com.example.lazaro.feature.register.RegisterViewModel
+import com.example.lazaro.feature.register.registerScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +20,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LazaroTheme {
-                LoginScreen().loginScreen()
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = NavRouter.LoginScreen.route
+                ){
+                    composable(NavRouter.LoginScreen.route){
+                        loginScreen(navController)
+
+                    }
+                    composable (NavRouter.RegisterScreen.route){
+                        val registerViewModel: RegisterViewModel = viewModel()
+                        registerScreen(navController, registerViewModel)
+                    }
+                }
             }
         }
     }
