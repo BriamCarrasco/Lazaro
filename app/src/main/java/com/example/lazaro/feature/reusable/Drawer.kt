733170f6a1f8b9320044586485d522ab1a9c76f0
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-09-12 04:03:32 -0300

    Se genera menú lateral para agregar opciones para el usuario

---
+++ app/src/main/java/com/example/lazaro/feature/reusable/Drawer.kt
@@ -0,0 +1,97 @@
+package com.example.lazaro.feature.reusable
+
+import android.util.Log
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import kotlinx.coroutines.CoroutineScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import com.example.lazaro.feature.session.SessionViewModel
+import androidx.compose.ui.platform.LocalContext
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun drawerHome(
+    topBar: @Composable (drawerState: DrawerState, scope: CoroutineScope) -> Unit,
+    content: @Composable (PaddingValues) -> Unit = {},
+    darkThemeEnabled: Boolean,
+    onToggleTheme: () -> Unit,
+    sessionViewModel: SessionViewModel,
+    navController: NavHostController
+
+) {
+    val context = LocalContext.current
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet (
+                modifier = Modifier.width(280.dp)
+            ){
+
+                val currentUser = sessionViewModel.currentUser.value
+
+                Log.d("Drawer", "Current user: $currentUser")
+                Text(
+                    text = if (currentUser != null) {
+                        "Bienvenido, ${currentUser.nombre} ${currentUser.apellidoP}"
+                    } else {
+                        "Bienvenido"
+                    },
+                    modifier = Modifier.padding(16.dp),
+                    color = MaterialTheme.colorScheme.primary,
+                )
+
+
+                ThemeSwitcherLottie(
+                    darkThemeEnabled = darkThemeEnabled,
+                    onToggleTheme = onToggleTheme
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Button(
+                    onClick = {
+                        sessionViewModel.logout()
+                        sessionViewModel.saveSession(context, null) // Borra la sesión
+                        navController.navigate("loginScreen") {
+                            popUpTo("homeScreen") { inclusive = true }
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text("Cerrar sesión")
+                }
+            }
+        }
+    ) {
+
+        Scaffold(
+            topBar = { topBar(drawerState, scope) }
+        ) { innerPadding ->
+            content(innerPadding)
+        }
+
+    }
+}
+
+
