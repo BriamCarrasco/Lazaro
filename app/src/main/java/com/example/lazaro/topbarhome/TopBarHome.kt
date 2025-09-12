4a6ebb2c2ddbc0698e534b020c4d66deb4e190ed
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-09-12 04:08:52 -0300

    Se genera topbar para pantalla home, con un boton para abrir menu lateral

---
+++ app/src/main/java/com/example/lazaro/topbarhome/TopBarHome.kt
@@ -0,0 +1,37 @@
+package com.example.lazaro.topbarhome
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.MaterialTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBarHome(
+    title: String,
+    onMenuClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    containerColor: Color = MaterialTheme.colorScheme.background
+){
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(onClick = onMenuClick) {
+                Icon(Icons.Default.Menu, contentDescription = "Abrir panel")
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = containerColor,
+            titleContentColor = Color.DarkGray
+        ),
+        modifier = modifier
+    )
+}
