b209883e3826ad314daaf91e3735db34ed5c9fdf
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-10-01 04:56:54 -0300

    Se agrega dependencia para dataStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
 
 
 }
