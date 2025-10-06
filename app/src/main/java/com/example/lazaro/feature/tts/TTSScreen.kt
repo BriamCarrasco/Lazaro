703c314c55116d7463c07f89dc2ef601b8941ac6
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-10-06 02:03:31 -0300

    Se codifica pantalla para pasar de texto a voz.

--- app/src/main/java/com/example/lazaro/feature/tts/TTSScreen.kt
+++ app/src/main/java/com/example/lazaro/feature/tts/TTSScreen.kt
@@ -1,24 +1,72 @@
 package com.example.lazaro.feature.tts
 
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import topBarBack
+import java.util.Locale
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Surface
+import androidx.compose.ui.text.font.FontWeight
+
 
 @Composable
 fun ttsscreen(navRouter: NavController, onBack: () -> Unit) {
+
+    val context = LocalContext.current
+    var textToSpeak by remember { mutableStateOf("Hola, pulsa el botón para escuchar este texto.") }
+
+    val textToSpeech = remember {
+        TextToSpeech(context, null)
+    }
+    LaunchedEffect(textToSpeech) {
+        val result = textToSpeech.setLanguage(Locale("es"))
+        val speechRate = 1f
+        textToSpeech.setSpeechRate(speechRate)
+    }
+
+    val speakText: (String) -> Unit = { text ->
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "tts_utterance_id")
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -41,6 +89,71 @@ fun ttsscreen(navRouter: NavController, onBack: () -> Unit) {
         ){
 
 
+            Text (
+                text = "Texto a voz",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.ExtraBold,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = textToSpeak,
+                onValueChange = { textToSpeak = it },
+                placeholder = {
+                    Text("Escribe el texto aquí",
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+
+                    )
+                },
+                modifier = Modifier.fillMaxWidth()
+                    .height(250.dp)
+                    .background(Color.White, shape = RoundedCornerShape(32.dp)),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = MaterialTheme.colorScheme.primary,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.outline,
+                    focusedTextColor = MaterialTheme.colorScheme.primary,
+                    unfocusedTextColor = MaterialTheme.colorScheme.onSurface,
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    cursorColor = MaterialTheme.colorScheme.primary,
+                    focusedPlaceholderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    unfocusedPlaceholderColor = MaterialTheme.colorScheme.onSurfaceVariant
+                ),
+                shape = RoundedCornerShape(32.dp),
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+            Button(
+                onClick = { speakText(textToSpeak) }
+            ) {
+                Text(
+                    text = "Reproducir Voz",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {textToSpeech.stop()},
+            ){
+                Text(
+                    text = "Parar Voz",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                )
+            }
+
         }
+
+
     }
 }
